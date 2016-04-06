40c9585b0cb313f249658ec605130b04c189d56c
Kristian Munter Simonsen <Mokkatroffen@gmail.com>
2016-04-06 14:36:10 +0200

    La inn kommentar for å lære meg git på IDE.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().
                 getResource("sample.fxml"));
-
+//main er thomas toget.
         Scene scene = new Scene(root);
 
         stage.setTitle("GOL");
