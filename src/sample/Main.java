68ae53a6cb266464b946a3991ee4a4be0adb7215
vimes90 <andreas1300@gmail.com>
2016-04-07 21:29:28 +0100

    Merge fra branch til master, kode testet.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,8 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().
                 getResource("sample.fxml"));
-//main er thomas toget.
-        //men hvorfor kommer ikke dette på github!?
+
         Scene scene = new Scene(root);
 
         stage.setTitle("GOL");
