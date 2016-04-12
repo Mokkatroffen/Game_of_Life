c3d244abd7e8a2f555ca0af17f4acbf8f5336c44
vimes90 <nestea>
2016-04-12 17:43:05 +0200

    refactored (endred navn) til sample.fxml til grafikk.fxml før det blir for vanskelig å endre. Enringer gjort i selve fxml filen og referanser i main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().
-                getResource("sample.fxml"));
+                getResource("grafikk.fxml"));
 
         Scene scene = new Scene(root);
 
