c766d44e89312c6e8d4fa575de55f9258b1c1609
vimes90 <andreas1300@gmail.com>
2016-04-17 17:33:33 +0200

    Merge fra branch til master, kode testet. Fungerer ikke, lagt til relParser.java for å lese rle-filer. Håndtering som printer ut kommentarer ferdig, kode for å lese f ilene ikke ferdig.
    Pushet med følgende feil i koden:
    http://i.imgur.com/NCck38c.png

--- src/sample/rleParser.java
+++ src/sample/rleParser.java
@@ -1,11 +1,13 @@
 package sample;
 
-import javax.swing.*; //JOption Dialogbox er en Swing-funkson. Bør rekodes til javaFX. **OBS OBS**
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+/**
+ * Created by andreas on 4/13/2016.
+ */
 public class rleParser {
 
     public void readGameBoard(BufferedReader r) throws IOException {
@@ -15,30 +17,20 @@ public class rleParser {
         String comment = new String();
         String line = new String();
 
-        StringBuilder melding = new StringBuilder();  //"Kommentar: " + comment + "\n Navn" + name + "Forfatter: " + author + "Kommentar: " + comment;
-
         while ((line = r.readLine()) != null) { //r er en buffered reader, sjekker på om det eksistere noe i BufferedReaderen.
             if (line.charAt(0) == '#') { //leser etter metadata i kommentarer her, åpner det og lagrer dataen om feks forfatter.
                 if (line.charAt(1) == 'C') {
                     comment = line;
-                    melding.append("Kommentar: " + comment + "\n");
                 } else if (line.charAt(1) == 'N') {
                     name = line;
-                    melding.append("Navn: " + line + "\n");
                 } else if (line.charAt(1) == 'O') {
-                    author = "forfatter" + line;
-                    melding.append("Forfatter: " + author + "\n");
-                } else if (line.charAt(1) == 'c') { //dette kan også være comment, sjekk i RLE filer og på life-wiki..
-                    comment = "kommentar" + line;
-                    melding.append("Kommentar: " + "\n");
+                    author = line;
+                } else if (line.charAt(1) == 'c') { //dette kan også være comment, sjekk i RLE filer og på life-wiki.
+                    comment = line;
                 }
-                //System.out.print(name + "\n" + comment + "\n"); // en test som printer eventuelle ting. KAN SLETTES, PRINTER KUN I TERMINAL.String melding = "Kommentar: " + comment + "\n Navn" + name + "Forfatter: " + author + "Kommentar: " + comment;
-                //JOptionPane.showMessageDialog(null, melding); <- feil med at den looper igjennom hele while-løkka
-
                 continue;
-
-
-            } else {
+            }
+            else {
                 //her kommer enten selve brettet eller metadata om størrelse på brettet i form av x, y og rule som er en divisjon.
                 //Pattern.compile er en en statisk metode som returnerer ett Pattern objekt, denne returnerer bare hva som kreves for å matche.
                 //så bruker vi en matcher senere for å kjøre patterne fra kompileren.
@@ -48,8 +40,6 @@ public class rleParser {
             }
 
         }
-        JOptionPane.showMessageDialog(null, melding);
-
 
     }
 
