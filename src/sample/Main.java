9037031eb570fc5f3f1dbf46e93d0396f99b0425
Kristian Munter Simonsen <Mokkatroffen@gmail.com>
2016-04-18 17:36:00 +0200

    Ferdig Javadocs for Main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,18 +7,27 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- * Main is the initiator of the the program Game Of Life n
+ * Main is the initiator of the the program Game Of Life
  *
  * @author Andreas Jacobsen
- *
- * @version 1.0 - April 18, 2016
+ * @version 0.2 - April 18, 2016
  *
  * For extended information about Application:
  * @see javafx.application.Application
  */
+
 public class Main extends Application {
     @Override
 
+/**
+ * The start method initialises the stage Stage while the Exception controls any exceptions.
+ *
+ * @author Boris Illievski
+ * @version 0.2 - April 18, 2016
+ *
+ * @see Exception
+ * @see Stage
+ */
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().
                 getResource("grafikk.fxml"));
