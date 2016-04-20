360b8fbfbe3c0c8ca891314c60dbb7319d651a60
Kristian Munter Simonsen <Mokkatroffen@gmail.com>
2016-04-20 11:14:26 +0200

    Komplett javadocs gode... igjen

--- src/sample/rleParser.java
+++ src/sample/rleParser.java
@@ -12,7 +12,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class rleParser {
-
+    /**
+     * readGameBoard reads the metadata and board.
+     *
+     * @author Andreas Jacobsen.
+     * @version 0.2 - April 18, 2016.
+     *
+     * @param r r is an buffered reader.
+     * @throws IOException IOException throws out an error message if needed.
+     */
     public void readGameBoard(BufferedReader r) throws IOException {
         //Skal kunne lese metadata og brett
         String author = new String();
@@ -85,7 +93,15 @@ public class rleParser {
         alert.showAndWait();
 
     }
-
+    /**
+     * readGameBoardFromDisk reades a file from disk onto board.
+     *
+     * @author Boris Illievski.
+     * @version 0.2 - April 18, 2016.
+     *
+     * @param file file is the file which is uploaded.
+     * @throws IOException IOException throws out an error message if needed.
+     */
     public void readGameBoardFromDisk(File file) throws IOException {
         if (!file.canRead()) {
             System.out.println("Kunne ikke lese fil.");
@@ -98,7 +114,15 @@ public class rleParser {
             System.out.println("Feilmelding for IOE-Feil");
         }
     }
-
+    /**
+     * readGameBoardFromUrl reads .rle file from an URL an implements it into the board.
+     *
+     * @author Kristian Munter Simonsen.
+     * @version 0.2 - April 18, 2016.
+     *
+     * @param url url is the parameter which you gather the file from.
+     * @throws IOException IOException throws out an error message if needed.
+     */
     public void readGameBoardFromURL(String url) throws IOException {
 
         try {
