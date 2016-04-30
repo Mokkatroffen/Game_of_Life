1d79f0b8fdca6b2022762a38051c5d9ba1bb3e3b
Andreas Jacobsen <andreas1300@gmail.com>
2016-04-30 15:34:46 +0200

    lagt til meh dynamicBoard

---
+++ src/GoL/dynamicBoard.java
@@ -0,0 +1,11 @@
+package GoL;
+
+/**
+ * Created by andreas on 30.04.16.
+ */
+public class dynamicBoard {
+
+    //gi arrayboard (array) med byte verdier
+    //Hver gang det er en celle i utkanten av brettet, så ekspanderer du.
+
+}
