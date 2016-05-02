6de96c354a04ef222689404cb5bc0d98c3209723
Kristian Munter Simonsen <Mokkatroffen@gmail.com>
2016-05-02 11:12:26 +0200

    minialt endringer, lagt litt dynamicBoard.java

--- src/GoL/dynamicBoard.java
+++ src/GoL/dynamicBoard.java
@@ -1,9 +1,14 @@
 package GoL;
 
+import java.util.List;
+
 /**
  * Created by andreas on 30.04.16.
  */
 public class dynamicBoard {
+
+    private List<List<Byte>> board;
+
 
     //gi arrayboard (array) med byte verdier
     //Hver gang det er en celle i utkanten av brettet, så ekspanderer du.
