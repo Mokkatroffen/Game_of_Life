669b58239a0eb16cd2347378b00f38e4bbfde223
Boris <hakon.ilievski@live.no>
2016-05-04 16:12:36 +0200

    icon på selve spillet done. gg wp hurra

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 /**
@@ -31,13 +34,20 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("GoL/View/grafikk.fxml"));
 
         Scene scene = new Scene(root, width, height);
-
+        addIcon(stage);
         stage.setTitle("Game Of Life");
         stage.setScene(scene);
         stage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
-
+    }
+    private void addIcon(Stage stage) {
+        Image image = new Image(this.getClass().getResource("GoL/View/GoL.gif").toString());
+        stage.getIcons().add(image);
+        ImageView imageIcon = new ImageView(image);
+        imageIcon.setFitWidth(50);
+        imageIcon.setPreserveRatio(true);
     }
 }
